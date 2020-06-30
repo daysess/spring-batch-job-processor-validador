58f1fe09a64c7bf657f13290c512223513eed289
Dayse Soares <daysedesign@gmail.com>
2020-06-30 20:44:23 -0300

    Novo job - validacao no processor

---
+++ src/main/java/com/springbatch/processadorvalidacao/processor/ProcessadorValidacaoProcessorConfig.java
@@ -0,0 +1,8 @@
+package com.springbatch.processadorvalidacao.processor;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProcessadorValidacaoProcessorConfig {
+	
+}
